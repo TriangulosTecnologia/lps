6618e4087ab8fb723169a3fe80d5e4cbefc38f25
Rayza Oliveira <rayza.ocr@gmail.com>
2021-07-19 19:48:48 -0300

    feat: exporting new icons

---
+++ lps/simulare/src/components/icons/Send.tsx
@@ -0,0 +1,27 @@
+const Send = () => {
+  return (
+    <svg
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M23.0644 2.97223L16.0102 23.0278L11.9792 14.0028L2.90948 9.99167L23.0644 2.97223Z"
+        stroke="#D9D9D9"
+        strokeWidth="2"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.0644 2.97223L11.9792 14.0028"
+        stroke="#D9D9D9"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Send;
---
+++ lps/simulare/src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as ArrowDown } from './ArrowDown';
+export { default as Email } from './Email';
+export { default as Perfil } from './Perfil';
+export { default as Whatsapp } from './Whatsapp';
+export { default as Send } from './Send';
