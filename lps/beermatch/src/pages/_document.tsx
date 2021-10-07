fbf17aa11a50be99f99f32d3b013fa63b167e889
Pedro Arantes <arantespp@gmail.com>
2021-10-07 16:58:18 -0300

    feat: add pixel configuration

--- lps/beermatch/src/pages/_document.tsx
+++ lps/beermatch/src/pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 // eslint-disable-next-line max-classes-per-file
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
@@ -8,6 +10,8 @@ class MyDocument extends Document {
   fonts: string[] = [];
 
   gtmId = '';
+
+  pixelId = '';
 
   // eslint-disable-next-line class-methods-use-this
   render() {
@@ -31,6 +35,22 @@ class MyDocument extends Document {
           {this.gtmId && (
             <script
               dangerouslySetInnerHTML={{
+                __html: `  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', ${this.pixelId});
+                  fbq('track', 'PageView');`,
+              }}
+            />
+          )}
+          {this.pixelId && (
+            <script
+              dangerouslySetInnerHTML={{
                 __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -51,6 +71,16 @@ class MyDocument extends Document {
               />
             </noscript>
           )}
+          {this.pixelId && (
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none', visibility: 'hidden' }}
+                src={`https://www.facebook.com/tr?id=${this.pixelId}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          )}
           <Main />
           <NextScript />
         </body>
@@ -67,6 +97,8 @@ class D extends MyDocument {
   ];
 
   gtmId = 'GTM-52XW83F';
+
+  pixelId = '449989473122281';
 }
 
 export default D;
