78cfd0d2718daceb09c026ec0a20880d49c705c9
Pedro Arantes <arantespp@gmail.com>
2021-10-16 12:54:28 -0300

    feat: add age modal

---
+++ lps/beermatch/src/components/AgeModal.tsx
@@ -0,0 +1,82 @@
+import { useRouter } from 'next/router';
+import * as React from 'react';
+import Modal from 'react-modal';
+import { Button, Flex, Heading, Text } from 'theme-ui';
+
+const AgeModal = () => {
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  const { push } = useRouter();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      style={{
+        overlay: {
+          zIndex: 10,
+          opacity: 1,
+          backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        },
+        content: {
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+          padding: 0,
+          height: 'fit-content',
+          width: '90%',
+          maxWidth: '600px',
+        },
+      }}
+      contentLabel="Example Modal"
+    >
+      <Flex>
+        <Heading
+          as="h4"
+          sx={{
+            width: '100%',
+            backgroundColor: 'secondary',
+            color: 'highlight',
+            textAlign: 'center',
+            padding: 7,
+          }}
+        >
+          Verificação de Idade
+        </Heading>
+      </Flex>
+      <Flex
+        sx={{
+          backgroundColor: 'primary',
+          height: 'auto',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Text sx={{ paddingX: 8, paddingY: 10, fontSize: 4 }}>
+          Este website comercializa bebidas alcóolicas e portanto deve ser
+          utilizado somente por maiores de 18 anos. Ao navegar em nosso site,
+          você atesta estar legalmente apto ao consumo de nossos produtos.
+        </Text>
+        <Flex
+          sx={{
+            flexDirection: ['column', 'row'],
+            gap: 8,
+            marginBottom: 10,
+            button: {
+              backgroundColor: 'secondary',
+            },
+          }}
+        >
+          <Button onClick={() => setIsOpen(false)}>
+            Tenho mais de 18 anos
+          </Button>
+          <Button onClick={() => push('https://www.leitejussara.com.br/')}>
+            Sou menor, me tire daqui
+          </Button>
+        </Flex>
+      </Flex>
+    </Modal>
+  );
+};
+
+export default AgeModal;
--- lps/beermatch/src/components/Layout.tsx
+++ lps/beermatch/src/components/Layout.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Container, Flex } from 'theme-ui';
 
+import AgeModal from './AgeModal';
 import Footer from './Footer';
 import Header from './Header';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Container>
-      <Flex
-        sx={{
-          position: 'relative',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Header />
-        {children}
-        <Footer />
-      </Flex>
-    </Container>
+    <>
+      <AgeModal />
+      <Container>
+        <Flex
+          sx={{
+            position: 'relative',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Header />
+          {children}
+          <Footer />
+        </Flex>
+      </Container>
+    </>
   );
 };
 
